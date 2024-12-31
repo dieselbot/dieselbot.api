c2b0e8e7de29588ee0febcd35cd45bed0ff5974d
EFS Helper <askmrtillman@gmail.com>
2024-12-31 02:15:11 -0500

    install redis; subscribe on server start

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
+const { createClient } = require('redis');
+
 const fastify = require('fastify')({
-    logger: true
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+// Run the server!
+fastify.listen({ port: 8080, host: '0.0.0.0' }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }``
+  // Server is now listening on ${address}
+})
+
+const client = createClient({
+  url: process.env.UPSTASH_REDIS_REST_URL
+});
+const subscriber = client.duplicate();
+
+subscriber
+.on('error', err => console.log('Redis Client Error', err))
+.connect().then(() => {
+  subscriber.subscribe('fuelstop.found', message => {
+    console.log(message)
   })
-  
-  // Declare a route
-  fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' })
-  })
-  
-  // Run the server!
-  fastify.listen({ port: 8080, host: '0.0.0.0' }, function (err, address) {
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
-    // Server is now listening on ${address}
-  })
+});
+
+
