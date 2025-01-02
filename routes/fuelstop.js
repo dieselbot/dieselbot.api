f8ff65c0a5bf8bbfab1bd2fc653aeea66676673e
EFS Helper <askmrtillman@gmail.com>
2025-01-02 01:59:25 -0500

    log incoming fuel stops

--- routes/fuelstop.js
+++ routes/fuelstop.js
@@ -1,6 +1,19 @@
-async function fuelstopRoute(request, reply){
-    console.log(request.body);
-    reply.send(request.body);
+// const FuelStopRepo = require("../efshelper.core/repository/fuelstop.repo");
+const FuelStop = require("../efshelper.core/domain/fuel.stop");
+
+async function fuelstopRoute(request, reply) {
+
+    const fuelstops = JSON.parse(request.body);
+    const added = [];
+
+    fuelstops.forEach(fuelstop => {
+        if (FuelStop.isValid(fuelstop)) {
+            console.log(fuelstop)
+            added.push(fuelstop);
+        }
+    })
+
+    reply.send(added);
 }
 
 module.exports = fuelstopRoute;
