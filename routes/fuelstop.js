d6ef3a2c8d8f3815f6c54ad22ac112bdd2303d16
EFS Helper <askmrtillman@gmail.com>
2025-01-04 14:25:13 -0500

    refactor to use repo plugin; send 400 response if fuel stop is invalid

--- routes/fuelstop.js
+++ routes/fuelstop.js
@@ -6,21 +6,18 @@ async function fuelstopRoute(request, reply) {
     let fuelstops = request.body;
 
     if(/text\/plain/.test(request.headers['content-type'])){
-        fuelstops = JSON.parse(request.body)
+        fuelstops = JSON.parse(request.body);
     }
 
-    const added = [];
-
-    const repo = new FuelStopRepo();
-
-    fuelstops.forEach(fuelstop => {
+    fuelstops.forEach((fuelstop, i) => {
         if (FuelStop.isValid(fuelstop)) {
-            repo.addOne(fuelstop);
-            added.push(fuelstop);
+            this.fuelStopRepo.addOne(fuelstop);
+        } else {
+            reply.code(400).send(`invalid fuel stop at index ${i}: ${JSON.stringify(fuelstop)}`);
         }
     })
 
-    reply.send(added);
+    reply.send();
 }
 
 module.exports = fuelstopRoute;
