bc07c53144ef0b4ba6bfccb47949c79115d3aef6
EFS Helper <askmrtillman@gmail.com>
2025-01-05 16:31:25 -0500

    Revert "use path.join to resolve fuel stop repo"
    This reverts commit f693b7367ab10ed46bf33d9e88373b2ce1c056b8.

--- plugins/fuelstop.repo.plugin.js
+++ plugins/fuelstop.repo.plugin.js
@@ -1,8 +1,5 @@
 const fastifyPlugin = require('fastify-plugin');
-const path = require('path');
-const FuelStopRepo = require(
-  path.join(__dirname, '../efshelper.core/repository/fuelstop.repo.js')
-);
+const FuelStopRepo = require('../efshelper.core/repository/fuelstop.repo.js');
 
 async function fuelStopRepoPlugin(fastify, options) {
   fastify.decorate('fuelStopRepo', new FuelStopRepo());
